5ced0588552c31f59999e870898b0ae226fa0df9
Muhammad Saad <113266888+saadiqbal603@users.noreply.github.com>
2025-07-08 00:43:41 +0500

    Initial Commit

---
+++ student.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Random;
+
+public class student {
+    protected String name[];
+    protected String father_name[];
+    protected int roll[];
+
+
+
+
+
+
+
+}
+
+
